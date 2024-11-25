c96b5dbfb6c2cc3e76f6def735265d6d31ce8317
Sergei Klokov <s2584835@ya.ru>
2024-11-25 17:30:53 +0300

    sem3

--- seminar/todoApp/src/main/java/ru/gb/ToDoListApp.java
+++ seminar/todoApp/src/main/java/ru/gb/ToDoListApp.java
@@ -1,7 +1,13 @@
 package ru.gb;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ToDoListApp {
     public static final String FILE_JSON = "tasks.json";
@@ -10,4 +16,53 @@ public class ToDoListApp {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final XmlMapper xmlMapper = new XmlMapper();
+
+    public static void addNewTask(Scanner scanner, List<ToDo> tasks) {
+        System.out.println("Введите название новой задачи:");
+        String newTaskTitle = scanner.nextLine();
+        tasks.add(new ToDo(newTaskTitle));
+        saveTasksToFile(FILE_JSON, tasks);
+        saveTasksToFile(FILE_BIN, tasks);
+        saveTasksToFile(FILE_XML, tasks);
+        System.out.println("Новая задача добавлена.");
+    }
+
+    public static void saveTasksToFile(String fileName, List<ToDo> tasks) {
+        try {
+            if (fileName.endsWith(".json")) {
+                objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+                objectMapper.writeValue(new File(fileName), tasks);
+            } else if (fileName.endsWith(".bin")) {
+                try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+                    oos.writeObject(tasks);
+                }
+            } else if (fileName.endsWith(".xml")) {
+//                String s = xmlMapper.writeValueAsString(tasks);
+                xmlMapper.writeValue(new File(fileName), tasks);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<ToDo> loadTasksFromFile(String fileName) {
+        List<ToDo> tasks = new ArrayList<>();
+
+        File file = new File(fileName);
+        if (file.exists()) {
+            try {
+                if (fileName.endsWith(".json")) {
+                    tasks = objectMapper.readValue(file, objectMapper.getTypeFactory().constructCollectionType(List.class, ToDo.class));
+                } else if (fileName.endsWith(".bin")) {
+                    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                        tasks = (List<ToDo>)ois.readObject();
+                    }
+                } else if (fileName.endsWith(".xml")) {
+                    tasks = xmlMapper.readValue(file, xmlMapper.getTypeFactory().constructCollectionType(List.class, ToDo.class));
+                }
+            } catch (IOException | ClassNotFoundException e) { e.printStackTrace(); }
+        }
+        return tasks;
+    }
 }
